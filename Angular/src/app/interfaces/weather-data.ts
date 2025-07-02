b283152c09f91ca206f4df6673e6c800a9e48185
BlazBele <106202319+BlazBele@users.noreply.github.com>
2025-07-02 13:28:54 +0200

    Update weather-data.ts

--- Angular/src/app/interfaces/weather-data.ts
+++ Angular/src/app/interfaces/weather-data.ts
@@ -1,9 +1,17 @@
 export interface WeatherData {
-    temperature: number;
-    humidity: number;
-    pressure0: number;
-    wind_speed: number;
-    wind_direction: string;
-    timestamp: string;
-    created_at: string;
+  temperature: number;      
+  humidity: number;         
+  pressure0: number;        
+  wind_speed: number;      
+  wind_direction: string;   
+  timestamp: string;
+  created_at: string;
+  //Dnevni
+  min_temperature?: number;
+  max_temperature?: number;
+  min_humidity?: number;
+  max_humidity?: number;
+  min_pressure?: number;
+  max_pressure?: number;
+  max_wind_speed?: number;
 }
